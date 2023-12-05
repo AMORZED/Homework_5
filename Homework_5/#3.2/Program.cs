86bd6d78af59187ebbd440716cd461857bb07bfe
Alexei Morozov <amorzedmail@gmail.com>
2023-12-05 23:22:25 +0300

    Homework done! (#5.3.2)

---
+++ Homework_5/#3.2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Homework_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+            int number = Factorial(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine($"\nФакториал этого числа: {number}");
+        }
+        static int Factorial(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n *= Factorial(n - 1);
+            }
+        }
+    }
+}
