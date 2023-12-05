c81a0dd5f520e9094c87c008ca3457c80192f3da
Alexei Morozov <amorzedmail@gmail.com>
2023-12-05 23:17:31 +0300

    Homework done! (#5.2)

---
+++ Homework_5/#2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Homework_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число, которое надо умножать:");
+            int baseNum = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите число, до которого будет выполняться умножение:");
+            int maxNum = int.Parse(Console.ReadLine());
+            Console.WriteLine(); //Просто пустая строка
+
+            for (int i = 1; i <= maxNum; i++)
+            {
+                int result = baseNum * i;
+                Console.WriteLine($"{baseNum}x{i}={result}");
+            }
+        }
+    }
+}
