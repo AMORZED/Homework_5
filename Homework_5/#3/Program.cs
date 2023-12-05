340f4bc76f995e138ad2632dcee563642eb69059
Alexei Morozov <amorzedmail@gmail.com>
2023-12-05 23:19:51 +0300

    Homework done! (#5.3)

---
+++ Homework_5/#3/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Homework_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите номер числа, которое надо найти:");
+            int number = Fibonacci(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine("\nРезультат: " + number);
+        }
+        static int Fibonacci(int i)
+        {
+            if (i == 1 | i == 2) return 1;
+            else return Fibonacci(i - 1) + Fibonacci(i - 2);
+        }
+    }
+}
