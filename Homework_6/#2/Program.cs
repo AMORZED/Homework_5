fa510d1d0dcbafe419236287ee8b3051d6cb9435
Alexei Morozov <amorzedmail@gmail.com>
2023-12-07 09:03:33 +0300

    Homework done! (#6.2)

---
+++ Homework_6/#2/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace _2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            Console.WriteLine("Введите количество строк в матрицах:");
+            int AmmountOfStrokes = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите количество столбцов в матрицах:");
+            int AmmountOfColumns = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Массив А:");
+            int[,] mass_A = new int[AmmountOfStrokes, AmmountOfColumns];
+            for (int i = 0; i < mass_A.GetLength(0); i++)
+            {
+             for (int j = 0; j < mass_A.GetLength(1); j++)
+                {
+                    mass_A[i, j] = rnd.Next(1, 11);
+                    Console.Write($"{mass_A[i,j]}\t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\nМассив B:");
+            int[,] mass_B = new int[AmmountOfStrokes, AmmountOfColumns];
+            for (int i = 0; i < mass_A.GetLength(0); i++)
+            {
+                for (int j = 0; j < mass_B.GetLength(1); j++)
+                {
+                    mass_B[i, j] = rnd.Next(1, 11);
+                    Console.Write($"{mass_B[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("\nРезультат:");
+            int[,] mass_C = new int[AmmountOfStrokes, AmmountOfColumns];
+            for(int i = 0;i < mass_C.GetLength(0);i++)
+            {
+                for(int j = 0;j < mass_C.GetLength(1); j++)
+                {
+                    mass_C[i,j] = mass_A[i,j] + mass_B[i,j];
+                    Console.Write($"{mass_C[i,j]}\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
