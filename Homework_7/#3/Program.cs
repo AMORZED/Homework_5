e8554148790e798af1f36f7e0ee4e0d749ec4c69
Alexei Morozov <amorzedmail@gmail.com>
2023-12-11 18:42:51 +0300

    Homework done! (#7.3)

---
+++ Homework_7/#3/Program.cs
@@ -0,0 +1,27 @@
+﻿using _Stack;
+using System.Security.Cryptography.X509Certificates;
+namespace Homework_7
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack st = new Stack(); //Объектная переменная для класса Stack
+
+            st.Show();
+            st.IsEmpty();
+            st.Push("IT Academy");
+            st.Show();
+            st.Remove(); st.Remove();
+            st.Show();
+            st.Remove();
+            st.Remove();
+            st.Remove();
+            st.Remove();
+            st.Remove();
+            st.Show();
+        }
+
+    }
+}
+          
---
+++ Homework_7/#3/Stack.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace _Stack
+{
+    public class Stack
+    {       
+        private object[] stack = new object[5] {"String", 27015, 15.32D, 'A', default(bool)};
+
+        public void Show() //Отображение текущего содержимого стека
+        {
+            Console.WriteLine("Текущее содержимое стека:");
+            for (int i = 0; i < stack.Length; i++) 
+            {
+                Console.Write(stack[i] + " | ");
+            }
+            Console.WriteLine("\n");
+        }
+
+        public void IsEmpty() //Проверка стека на наличие элементов
+        {
+            if (stack.Length == 0 | stack == null) Console.WriteLine("Стек пуст!");
+            else Console.WriteLine("Стек не пуст!");
+            Console.WriteLine();
+        }
+
+        public void Push(object newMember) //Запись нового элемента в стек
+        {
+            Array.Resize(ref stack, stack.Length + 1);
+            stack[stack.Length - 1] = newMember;
+        }
+
+        public void Remove() //Удаление одного элемента из стека
+        {            
+            if (stack.Length == 0) Console.WriteLine("Стек уже пуст!");
+            else Array.Resize(ref stack, stack.Length - 1);
+        }
+    }
+}
