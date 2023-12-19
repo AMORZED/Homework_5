cca4521ad8cbe3343decbf45732099f24a677eda
Alexei Morozov <amorzedmail@gmail.com>
2023-12-19 11:16:45 +0300

    - Homework done! (#9.1) - Homework done! (#9.2)

---
+++ Homework_9/#1/HerbivoreAnimal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _Animal;
+
+namespace _HerbivoreAnimal
+{
+    public class HerbivoreAnimal : Animal
+    {
+        private Animal animal;
+
+        public HerbivoreAnimal(Animal animal)
+        {
+            this.animal = animal;
+        }
+        public override void Live()
+        {
+            Console.WriteLine("В лесу появились травоядные!");
+            this.Bite();
+        }
+        public void Bite()
+        {
+            Console.WriteLine("Травоядные едят траву! Не мешайте им!");
+        }
+    }
+}
---
+++ Homework_9/#2/#2/Animal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _Herbivores;
+using _Predators;
+using _Zoo;
+
+namespace _Animal
+{
+    public class Animal
+    {
+        public string name;
+        public object typeOfAnimal;
+        
+        public Animal(Herbivores herbivore) 
+        { 
+            this.name = herbivore.name;
+            this.typeOfAnimal = herbivore.GetType();
+        }
+
+        public Animal(Predators predator) 
+        {
+            this.name = predator.name;
+            this.typeOfAnimal = predator.GetType();
+        }
+
+        public Animal() 
+        { 
+        
+        }
+    }
+
+
+}
