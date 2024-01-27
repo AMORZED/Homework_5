6df376070654f7a778a167b093a2c7f930072e43
Alexei Morozov <amorzedmail@gmail.com>
2024-01-27 16:48:27 +0300

    - Homework done! (#14)

---
+++ Homework_14/#1/Seller.cs
@@ -0,0 +1,52 @@
+﻿// <copyright file="Seller.cs" company="Alexei Morozov">
+// Copyright (C) Alexei Morozov. All rights reserved.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSeller
+{
+	/// <summary>
+	/// Класс продавца.
+	/// </summary>
+	internal class Seller
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Seller"/> class.
+		/// </summary>
+		/// <param name="name">Имя продавца.</param>
+		public Seller(string name)
+		{
+			if (name == null)
+			{
+				throw new ArgumentNullException("Необходимо задать продавцу имя!");
+			}
+			else
+			{
+				this.name = name;
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets name of seller.
+		/// </summary>
+		public string Name
+		{
+			get
+			{
+				return this.name;
+			}
+
+			set
+			{
+				this.name = value;
+			}
+		}
+
+		private string name;
+	}
+}
---
+++ Homework_14/#3/EmptyStringException.cs
@@ -0,0 +1,38 @@
+﻿// <copyright file="EmptyStringException.cs" company="Alexei Morozov">
+// Copyright (C) Alexei Morozov. All rights reserved.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMyException
+{
+	/// <summary>
+	/// Класс с пользовательским исключением.
+	/// </summary>
+	internal class EmptyStringException : Exception
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="EmptyStringException"/> class.
+		/// </summary>
+		/// <param name="message">Выводимое сообщение об ошибке по дефолту.</param>
+		public EmptyStringException()
+			: base(message)
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="EmptyStringException"/> class.
+		/// </summary>
+		/// <param name="text">Кастомное сообщение об ошибке.</param>
+		public EmptyStringException(string text)
+			: base(text)
+		{
+		}
+
+		private static string message = "Ошибка! Введена пустая строка!";
+	}
+}
